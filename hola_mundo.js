c422cd5090eff263f900da2fe689380f019be57d
Abel Rivero <abel.rivero.herrera>
2025-07-19 15:42:24 -0500

    este es mi primer codigo, y este es mi primer commit

---
+++ hola_mundo.js
@@ -0,0 +1,24 @@
+console.log("hola al mundo de la programacion")
+console.log("este es mi primer codigo en mi vida")
+var a=5;
+var b=8;
+var c=0;
+c=a+b;
+console.log("el valor de a es: "+a);
+console.log("el valor de b es: "+b);
+console.log("el resultado de la suma de a+b es: "+c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
