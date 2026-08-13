79831ed854ac93f145d5239f76ae45f9fbc0d9b8
programadorbmx <abel.rivero.herrera@gmail.com>
2026-08-13 08:42:57 -0500

    agregue 5 lineas nuevas de consola

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,1 +1,6 @@
-console.log("hola al bmx")
+console.log("hola al bmx");
+console.log("soy abel rivero");
+console.log("el bmx es divertido");
+console.log("el bmx tiene velocidad");
+console.log("la velocidad me  encanta");
+console.log("los partidores me facinan");
